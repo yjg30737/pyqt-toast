3e9f82441882bfc119009ee84f8b6b7e411167af
Jung Gyu Yoon <yjg30737@gmail.com>
2021-12-26 11:55:23 +0900

    Add showing, closing animation and make it transparent for mouse event (mouse event for main widget couldn't be blocked by this toast window)

--- pyqt_toast/toast.py
+++ pyqt_toast/toast.py
@@ -1,16 +1,19 @@
 from PyQt5.QtWidgets import QLabel, QWidget, QHBoxLayout, QGraphicsOpacityEffect
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtCore import Qt, QTimer, QPropertyAnimation, QAbstractAnimation
 
 
 class Toast(QWidget):
     def __init__(self, text, close_sec=2, parent=None):
         super().__init__(parent)
         self.__parent = parent
+        self.__timer = QTimer(self)
         self.__close_sec = close_sec
         self.__initUi(text)
 
     def __initUi(self, text):
-        self.setWindowFlags(Qt.FramelessWindowHint | Qt.SubWindow | Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.SubWindow)
+        self.setAttribute(Qt.WA_TransparentForMouseEvents, True)
+
         self.__lbl = QLabel(text)
         self.__lbl.setObjectName('popupLbl')
         self.__lbl.setStyleSheet('QLabel#popupLbl { color: #EEE; padding: 5px; }')
@@ -18,20 +21,31 @@ class Toast(QWidget):
         self.__lbl.setMinimumHeight(self.__lbl.fontMetrics().boundingRect(text).height()*2)
         self.__lbl.setWordWrap(True)
 
+        self.__animation = QPropertyAnimation(self, b"opacity")
+        self.__animation.setStartValue(0.0)
+        self.__animation.setDuration(200)
+        self.__animation.setEndValue(0.5)
+        self.__animation.valueChanged.connect(self.__setOpacity)
+
+        self.setGraphicsEffect(QGraphicsOpacityEffect(opacity=0.0))
+
         lay = QHBoxLayout()
         lay.addWidget(self.__lbl)
         lay.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
 
         self.setStyleSheet('QWidget { background: #444; border-radius: 5px; }')
-        self.setGraphicsEffect(QGraphicsOpacityEffect(opacity=0.5))
 
-        self.setMinimumWidth(self.__lbl.sizeHint().width()*2)
-        self.setMinimumHeight(self.__lbl.sizeHint().height()*2)
+        self.setFixedWidth(self.__lbl.sizeHint().width()*2)
+        self.setFixedHeight(self.__lbl.sizeHint().height()*2)
         self.setLayout(lay)
 
+    def __setOpacity(self, opacity):
+        opacity_effect = QGraphicsOpacityEffect(opacity=opacity)
+        self.setGraphicsEffect(opacity_effect)
+
     def __initTimeout(self, close_sec):
-        self.__timer_to_wait = close_sec
         self.__timer = QTimer(self)
+        self.__timer_to_wait = close_sec
         self.__timer.setInterval(1000)
         self.__timer.timeout.connect(self.__changeContent)
         self.__timer.start()
@@ -39,17 +53,21 @@ class Toast(QWidget):
     def __changeContent(self):
         self.__timer_to_wait -= 1
         if self.__timer_to_wait <= 0:
-            self.close()
-
-    def showEvent(self, e):
-        self.__initTimeout(self.__close_sec)
-        return super().showEvent(e)
+            self.__animation.setDirection(QAbstractAnimation.Backward)
+            self.__animation.start()
+            self.__timer.stop()
 
     def setPosition(self, pos):
         geo = self.geometry()
         geo.moveCenter(pos)
         self.setGeometry(geo)
 
-    def closeEvent(self, event):
-        self.__timer.stop()
-        event.accept()
+    def show(self):
+        if self.__timer.isActive():
+            pass
+        else:
+            self.__animation.setDirection(QAbstractAnimation.Forward)
+            self.__animation.start()
+            self.raise_()
+            self.__initTimeout(self.__close_sec)
+        return super().show()
