ec04fbfd27d1dd19f3be286eb9a6ade5264e20f7
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-14 09:35:34 +0900

    Add stylesheets content, add new package (pyqt-resource-helper)

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     description='PyQt Toast (Small message displayed on the screen, visible for a short time)',
     url='https://github.com/yjg30737/pyqt-toast.git',
     install_requires=[
-        'PyQt5>=5.8'
+        'PyQt5>=5.8',
+        'pyqt-resource-helper @ git+https://git@github.com/yjg30737/pyqt-resource-helper.git@main'
     ]
 )
